115cee3b25e0cc8fac57ebba19b493d149c4bd0e
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2022-12-02 16:08:49 -0400

    Katex instalation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KatexModule } from 'ng-katex';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { TestsModule } from './pages/tests/tests.module';
     AppRoutingModule,
     ComponentsModule,
     PagesModule,
-    TestsModule
+    TestsModule,
+    KatexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
