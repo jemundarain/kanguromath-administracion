80c35ddb0be10566cccc1ee992070bea04351677
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2022-12-02 17:30:02 -0400

    Primeros avances Edit Problem Component

--- src/app/pages/tests/edit-problem/edit-problem.component.ts
+++ src/app/pages/tests/edit-problem/edit-problem.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+interface City {
+  name: string,
+  code: string
+}
 @Component({
   selector: 'app-edit-problem',
   templateUrl: './edit-problem.component.html',
@@ -8,8 +11,25 @@ import { Component, OnInit } from '@angular/core';
 export class EditProblemComponent implements OnInit {
 
   constructor() { }
-
+  selectedValue: string = 'val1';
+  value1: number = 0;
+  min: number = 0;
+  max: number = 10;
+  property: string = '';
   ngOnInit(): void {
   }
+
+  selectedCity: City = {
+    name: "",
+    code: ""
+  };
+
+  cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+  ];
 
 }
