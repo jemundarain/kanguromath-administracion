4ddeab3ab12d5f7eb9336d72d7148d984089c03f
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2022-12-02 17:30:46 -0400

    Botón de cerrar sesión en el menú

--- src/app/components/slide-menu/slide-menu.component.ts
+++ src/app/components/slide-menu/slide-menu.component.ts
@@ -17,38 +17,43 @@ export class SlideMenuComponent implements OnInit {
     this.items = [
       {
         label: 'Informe',
-        icon: 'pi pi-fw pi-th-large',
+        icon: 'pi pi-th-large',
         routerLink: 'dashboard'
       },
       {
         label: 'Pruebas',
-        icon: 'pi pi-fw pi-file',
+        icon: 'pi pi-file',
         items: [{
           label: 'Todas las Pruebas', 
-          icon: 'pi pi-fw pi-file',
+          icon: 'pi pi-file',
           routerLink: 'pruebas'
         },
         {
           label: 'Prueba Nueva', 
-          icon: 'pi pi-fw pi-plus',
+          icon: 'pi pi-plus',
           routerLink: 'pruebas/prueba-nueva'
         },
         {
           label: 'Editar Prueba', 
-          icon: 'pi pi-fw pi-pencil',
+          icon: 'pi pi-pencil',
           routerLink: 'pruebas/editar-prueba'
         },
         {
           label: 'Editar problema', 
-          icon: 'pi pi-fw pi-pencil',
+          icon: 'pi pi-pencil',
           routerLink: 'pruebas/editar-problema'
         }]
       },
       {
         label: 'Ajustes',
-        icon: 'pi pi-fw pi-cog',
+        icon: 'pi pi-cog',
         routerLink: 'ajustes'
       },
+      {
+        label: 'Cerrar Sesión',
+        icon: 'pi pi-sign-out',
+        routerLink: '**'
+      }
     ];
   }
 
