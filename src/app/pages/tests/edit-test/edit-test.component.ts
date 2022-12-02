d3a26ba08fe4ebfe3e94224bfb312e6e3c2d28c4
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2022-12-02 16:11:54 -0400

    Primeros avances EditTest Component

--- src/app/pages/tests/edit-test/edit-test.component.ts
+++ src/app/pages/tests/edit-test/edit-test.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+interface City {
+  name: string,
+  code: string
+}
 @Component({
   selector: 'app-edit-test',
   templateUrl: './edit-test.component.html',
@@ -11,5 +14,22 @@ export class EditTestComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  value1: number = 0;
+  min: number = 0;
+  max: number = 10;
+
+  selectedCity: City = {
+    name: "",
+    code: ""
+  };
+
+  cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+  ];
+  
 
 }
