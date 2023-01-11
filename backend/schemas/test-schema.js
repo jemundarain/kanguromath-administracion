07871195347ccefea6c2b793c84df1967674ee79
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-11 11:19:49 -0400

    Cambio del tipo de campo de Levels, de [string] a string

--- backend/schemas/test-schema.js
+++ backend/schemas/test-schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const testSchema = mongoose.Schema({
     test_id: {type: String, required: true, unique: true},
-    levels: {type: [String], required: true},
+    levels: {type: String, required: true},
     edition: {type: String, required: true},
     problems: {type: [String], required: true}
 });
