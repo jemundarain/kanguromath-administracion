1705e2e11c4f22f5dbddab88892a56f5699dbebc
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-11 11:16:19 -0400

    Creación del pipe para mostrar los niveles en el encabezado de la prueba

---
+++ src/app/tests/pipes/levels-header.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'levelsHeader'
+})
+export class LevelsHeaderPipe implements PipeTransform {
+
+  transform(levels: string): string {
+    switch (levels) {
+      case '1ero':
+        return 'PRIMER AÑO'
+        break;
+      case '1ero-2do':
+        return 'PRIMER Y SEGUNDO AÑO'
+        break;
+      case '2do':
+        return 'PRIMER Y SEGUNDO AÑO'
+        break;
+      case '3ero':
+        return 'TERCER AÑO'
+        break;
+      case '4to':
+        return 'CUARTO AÑO'
+        break;
+      case '4to-5to':
+        return 'CUARTO Y QUINTO AÑO'
+        break;
+      case '5to':
+        return 'QUINTO AÑO'
+        break;
+    
+      default:
+        return levels;
+        break;
+    }
+  }
+}
