b998a639a2ed05e9d0510da29c12350c33fcc101
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-13 19:58:36 -0400

    Conexión con la api del edit-test component

--- src/app/tests/edit-test/edit-test.component.ts
+++ src/app/tests/edit-test/edit-test.component.ts
@@ -19,17 +19,14 @@ export class EditTestComponent implements OnInit {
   levels: LevelOption[];
   selectedLevelCode: string;
 
-  testForm: FormGroup;
-  test: Test;
-  testSub = new Subscription();
-  private paramId: string;
+  test!: Test;
 
   constructor(private testService: TestService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.levels = [
       {name: '1ero', code: '1ero'},
-      {name: '1ero y 2do', code: '1ero-2d0'},
+      {name: '1ero y 2do', code: '1ero-2do'},
       {name: '2do', code: '2do'},
       {name: '3ero', code: '3ero'},
       {name: '4to', code: '4to'},
@@ -37,30 +34,13 @@ export class EditTestComponent implements OnInit {
       {name: '5to', code: '5to'}
     ]
 
-    // this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-    //   if(paramMap.has('id')){
-    //     this.paramId = paramMap.get('id')!;
-    //     this.testService.getTestById(this.paramId);
-    //     this.testSub = this.testService.testSubject.subscribe(test => {
-    //       this.test = test;
-    //     });
-    //   }
-    // })
-
     this.activatedRoute.params
       .pipe(
         switchMap( ({ id }) => this.testService.getTestById(id))
       )
-      .subscribe( test => this.test = test );
-    
-    this.testForm = new FormGroup({
-      edition: new FormControl( 2020, [Validators.required]),
-      level: new FormControl(  '', [Validators.required])
-    })
+      .subscribe( test => this.test = test[0] );
   }
   
   onSubmit(){
-    const test = new Test('', this.testForm.value.levels, this.testForm.value.edition, ['']);
-    console.log(this.paramId);
   }
 }
