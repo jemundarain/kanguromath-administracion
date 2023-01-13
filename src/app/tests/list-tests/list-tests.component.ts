aa9e40a0be4b22a8840986b3913845249d1b4ac8
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-13 19:57:07 -0400

    Cambios en el list-test component

--- src/app/tests/list-tests/list-tests.component.ts
+++ src/app/tests/list-tests/list-tests.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControlName, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { TestService } from '../services/test.service';
@@ -12,47 +12,38 @@ import { Test } from '../test-model';
 export class ListTestsComponent implements OnInit, OnDestroy {
 
   constructor(
-    private activateRoute: ActivatedRoute,
     private testService: TestService,
     private router: Router,
     private formBuilder: FormBuilder
   ) { }
 
   form: FormGroup = new FormGroup({});
-  
-  tests: Test[] = [];
-  testsSub = new Subscription();
-
   editions: string[];
-  editionSub = new Subscription();
+  tests: Test[] = [];
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       edition: [null, Validators.nullValidator]
     });
 
-    this.testsSub = this.testService.testsSubject.subscribe(tests => {
-      this.tests = tests;
-    });
+    this.testService.getEditions()
+      .subscribe( editions => this.editions = editions);
 
-    this.testService.getEditions();
-    this.editionSub = this.testService.editionSubject.subscribe(editions => {
-      this.editions = editions;
-      this.form.valueChanges.subscribe((data) => {
-        this.testService.getTestsByEdition(data['edition']);
-      })
-    });
+    this.form.valueChanges.subscribe((data) => {
+      this.testService.getTestsByEdition(data['edition'])
+        .subscribe( tests => this.tests = tests);
+    })
   }
 
   ngOnDestroy(): void {
-    this.testsSub.unsubscribe();
+    
   }
 
-  onEdit(id: string){
+  /*onEdit(id: string){
     this.router.navigate(['pruebas/editar/',id])
   }
 
-  /*onDelete(id: string){
+  onDelete(id: string){
     this.testService.onDeleteTest(id);
   }
 
