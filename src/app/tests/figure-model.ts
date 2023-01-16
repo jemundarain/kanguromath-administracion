7474a0f5b4898e22302165484282e1063d9a08d2
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-16 10:50:14 -0400

    Problem y figure models

---
+++ src/app/tests/figure-model.ts
@@ -0,0 +1,5 @@
+export class Figure {
+
+    public constructor(public _id: string, public num_s: number, public url: string, public ubication: string) {}
+
+}
--- src/app/tests/problem-model.ts
+++ src/app/tests/problem-model.ts
@@ -1,7 +1,8 @@
 import { Option } from "./option-model";
+import { Figure } from "./figure-model"
 
 export class Problem {
 
-    public constructor(public num_s: number, public statement: string, public solution: string, public type: string, public url_image: string, public category: string, public options: Option[]) {}
+    public constructor(public _id: string, public problem_id: string, public num_s: number, public statement: string, public solution: string, public category: string, public options: Option[], public figures: Figure[]) {}
 
 }
