9ed43b5db817c94d4df016e3b5ed7fd27149aff8
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-16 10:50:56 -0400

    Creación e inclusión de Figures table component

--- src/app/tests/figures-table/figures-table.component.ts
+++ src/app/tests/figures-table/figures-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Figure } from '../figure-model';
 
 @Component({
   selector: 'app-figures-table',
@@ -8,7 +9,13 @@ export class FiguresTableComponent implements OnInit {
 
   constructor() { }
 
+  @Input() figures: Figure[]
+
   ngOnInit(): void {
+    console.log(this.figures);
   }
 
+  onBasicUpload() {
+    
+  }
 }
