52fbdc4b48cf349b8142e44f6e78ae3b618b5fc6
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-17 19:22:03 -0400

    Creación del pages service

---
+++ src/app/pages/services/pages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PagesService {
+
+  private baseUrl: string = environment.baseUrl;
+  constructor(private http: HttpClient){}
+
+  getNumberUsersByDateRange(start: string, end: string): Observable<number> {
+    return this.http.get<number>(`${ this.baseUrl }/usuarios?start=${ start }&end=${ end }`)
+  }
+
+}
