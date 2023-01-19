59b587b19335bd27f1884bbc5a5fad72b6fc6a46
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-19 15:51:41 -0400

    Traducciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,18 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private primengConfing: PrimeNGConfig) {}
+  constructor(private primengConfing: PrimeNGConfig) {}  
   title = 'administrador-canguromath-app';
 
   ngOnInit() {
     this.primengConfing.ripple = true;
+    this.primengConfing.setTranslation({
+      monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+      monthNamesShort: [ "ene","feb","mar","abr","may","jun", "jul","ago","sep","oct","nov","dic" ],
+      dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+      dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+      dayNamesMin: [ "D","L","M","X","J","V","S" ],
+      dateFormat: "dd/mm/yy",
+  });
   }
 }
