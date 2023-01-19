767e01ff7712a01ca03ff87a8841d52e11ba0246
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-19 17:24:52 -0400

    Ordenar alfabéticamente los componentes

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,40 +2,41 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { DropdownModule } from 'primeng/dropdown';
 import { FileUploadModule } from 'primeng/fileupload';
+import { ImageModule } from 'primeng/image';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { InputSwitchModule } from 'primeng/inputswitch';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { RadioButtonModule } from 'primeng/radiobutton';
-import { SlideMenuModule } from 'primeng/slidemenu';
-import { TableModule } from 'primeng/table';
-
-import {InputSwitchModule} from 'primeng/inputswitch';
-import { ImageModule } from 'primeng/image';
-import { CalendarModule } from 'primeng/calendar';
 import { SelectButtonModule } from 'primeng/selectbutton';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { StepsModule } from 'primeng/steps';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   exports: [
-    FormsModule,
     ButtonModule,
+    CalendarModule,
     CardModule,
     DropdownModule,
     FileUploadModule,
+    FormsModule,
+    ImageModule,
     InputNumberModule,
+    InputSwitchModule,
     InputTextareaModule,
     InputTextModule,
     PasswordModule,
     RadioButtonModule,
+    SelectButtonModule,
     SlideMenuModule,
-    TableModule,
-    InputSwitchModule,
-    ImageModule,
-    CalendarModule,
-    SelectButtonModule
+    StepsModule,
+    TableModule
   ]
 })
 export class PrimeNgModule { }
