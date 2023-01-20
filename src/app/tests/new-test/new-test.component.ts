b820b13af889d0b65ca29a6e6e72cd8fd052537e
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-19 23:00:21 -0400

    Separación por tipo de subida: automática o manual

--- src/app/tests/new-test/new-test.component.ts
+++ src/app/tests/new-test/new-test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LevelOption } from '../interfaces/level-option.interface';
+import { Test } from '../test-model';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-new-test',
@@ -8,21 +10,47 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class NewTestComponent implements OnInit {
 
   value1: number = 0;
-  min: number = 0;
-  max: number = 10;
-  
-  newTestForm: FormGroup;
+  min: number = 2002;
+  currentYear: string = new Date().getFullYear().toString();
+
+  test: Test = new Test('', '', this.currentYear, ['']);
+
+  levels: LevelOption[];
+  selectedLevelCode: string;
+
+  items: MenuItem[];
+
+  options: string[];
+  option: string = 'Manual';
 
   ngOnInit(): void {
-    this.newTestForm = new FormGroup({
-      "name": new FormControl(null, [Validators.required])
-    });    
+    this.options = [
+      'Automática',
+      'Manual'
+    ]
+    this.levels = [
+      {name: '1ero', code: '1ero'},
+      {name: '1ero y 2do', code: '1ero-2do'},
+      {name: '2do', code: '2do'},
+      {name: '3ero', code: '3ero'},
+      {name: '4to', code: '4to'},
+      {name: '4to y 5to', code: '4to-5to'},
+      {name: '5to', code: '5to'}
+    ]
+
+    this.items = [
+      {label: 'Step 1'},
+      {label: 'Step 2'},
+      {label: 'Step 3'}
+  ];
   }
 
-  onSubmit() {
-    
+  onBasicUpload() {    
   }
-  
+
+  addTest(test: Test) {
+
+  }
   // uploadedFiles: any[] = [];
 
   // constructor(private messageService: MessageService) {}
