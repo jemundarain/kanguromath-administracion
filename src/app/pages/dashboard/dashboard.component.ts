d8575092fd9c9cadd083efe2cd2fd3300304a21b
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-21 00:18:02 -0400

    Filtro por fecha del número de usuarios

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
 import { DateOption } from '../interfaces/date-option.interfaces';
 import { PagesService } from '../services/pages.service';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
+import { GlobalConstants } from 'src/app/common/global-constants';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,30 +15,45 @@ export class DashboardComponent implements OnInit {
 
   constructor(private pagesService: PagesService, private router: Router, private activatedRoute: ActivatedRoute, private fb: FormBuilder) { }
   
-  
-  public rangoFecha: string = 'Últimos 30 días: Enero 21 - Febrero 22, 2022';
   numberUsers: number;
   dateRange: Date[];
   dates: Date[]
   minDate: Date;
   maxDate: Date;
-  dateOption: string = 'customize';
-  dateOptions: DateOption[] = [
-    {name: 'Ayer', code: 'yesterday'},
-    {name: 'Últimos 7 días', code: 'last-7days'},
-    {name: 'Últimos 30 días', code: 'last-30days'},
-    {name: 'Rango de fecha personalizado', code: 'customize'},
-  ]
+  dateStart: string;
+  dateOption: string;
+  dateOptions: DateOption[] = GlobalConstants.DATE_OPTIONS;
+  daysBack: number;
+  @ViewChild('dateFilterForm', { static: true }) dateFilterForm!: NgForm;
 
   ngOnInit(): void {
-
-    this.minDate = new Date("2022-01-01"); //buscar en la api el día que se registró el 1er usuario
     this.maxDate = new Date();
-    // this.dateFilterForm.valueChanges.subscribe((data) => {
-    //   console.log('data', data);
-    //   this.pagesService.getNumberUsersByDateRange("2023-01-01", "2023-01-05")
-    //   .subscribe( numberUsers => this.numberUsers = numberUsers);
-    // })
-
+    this.pagesService.getMinimumRegistrationDate().subscribe((data) => {
+      this.minDate = new Date(data);
+    });
+    this.dateFilterForm.form.valueChanges.subscribe((data) => {
+      switch (this.dateFilterForm?.form.value.dateOption) {
+        case GlobalConstants.DATE_OPTIONS[0].code: //yesterday
+          this.daysBack = 1;
+        break;
+        case GlobalConstants.DATE_OPTIONS[1].code: //last-7days
+          this.daysBack = 7;
+        break;
+        case GlobalConstants.DATE_OPTIONS[2].code: //last-30days
+          this.daysBack = 30;
+        break;
+        case GlobalConstants.DATE_OPTIONS[3].code: //customize
+        this.pagesService.getNumberUsersByDateRange(data?.dates[0], data?.dates[1])
+          .subscribe( numberUsers => this.numberUsers = numberUsers );
+        break;
+        default:
+          break;
+      }
+      if(this.dateFilterForm?.form.value.dateOption != GlobalConstants.DATE_OPTIONS[3].code){
+        this.dateStart = (new Date(new Date().setDate(new Date().getDate() - this.daysBack ))).toISOString().split('T')[0];
+        this.pagesService.getNumberUsersByDateRange(this.dateStart, new Date().toISOString().split('T')[0])
+          .subscribe( numberUsers => this.numberUsers = numberUsers );
+      }
+    })
   }
 }
