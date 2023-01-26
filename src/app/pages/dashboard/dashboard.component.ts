d27084989a00523830389bdd96216a3f8bb66eff
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-26 06:56:31 -0400

    Formato e implementación inicial del exportar pdf

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartDataset, DatasetChartOptions } from 'chart.js';
 import { DateOption } from '../interfaces/date-option.interfaces';
 import { PagesService } from '../services/pages.service';
@@ -8,16 +8,21 @@ import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 import { GlobalConstants } from 'src/app/common/global-constants';
 import { Ranking } from '../interfaces/ranking.interfaces';
 
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private pagesService: PagesService, private router: Router, private activatedRoute: ActivatedRoute, private fb: FormBuilder) { }
+  constructor(private pagesService: PagesService, private router: Router, private activatedRoute: ActivatedRoute, private fb: FormBuilder) { 
+  }
   
   @ViewChild('dateFilterForm', { static: true }) dateFilterForm!: NgForm;
-
+  @ViewChild('report') report!: ElementRef;
+  
   //Usuarios Registrados
   numberUsers: number;
 
@@ -125,5 +130,33 @@ export class DashboardComponent implements OnInit {
       }
     }
     return dateOptions;
+  }
+
+  public openPDF(): void {
+
+    // Extraemos el
+    const DATA = document.getElementById('report');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    if(DATA) {
+      html2canvas(DATA, options).then((canvas) => {
+
+        const img = canvas.toDataURL('image/PNG');
+
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+        return doc;
+      }).then((docResult) => {
+        docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      });
+    }
   }
 }
