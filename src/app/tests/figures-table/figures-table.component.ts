f31a76b23c9776abbf796e87f61bc665cb4616ae
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-01-28 18:52:39 -0400

    Solución de errores en el edit problem y comienzo de implementación del upload

--- src/app/tests/figures-table/figures-table.component.ts
+++ src/app/tests/figures-table/figures-table.component.ts
@@ -10,15 +10,35 @@ export class FiguresTableComponent implements OnInit {
 
   constructor( private testService: TestService) { }
 
-  @Input() figures: Figure[];
+  @Input() figures: Figure[] = [];
   position: string;
+  contents: any = null;
+  filename: string;
 
   ngOnInit(): void {
-    this.testService.getProblemById
   }
 
-  onBasicUpload() {
-    
+  public upload(event: any, uploadFigure: any) {
+    console.log('Reading file...');
+    for (const file of event.files) {
+      const dataset = this.readFile(file);
+      console.log('onUpload: ', dataset);
+    }
+    uploadFigure.clear();
+  }
+
+  uploadFigure(event: any) {
+    console.log("Multiple Files are uploaded: ", event.files);
+  }
+
+  private readFile(file: File) {
+    const reader: FileReader = new FileReader();
+    reader.onload = () => {
+        console.log('readFile: ', reader.result);
+        this.contents = reader.result;
+    };
+    reader.readAsText(file);
+    this.filename = file.name;
   }
 
   setPosition(position: string){
