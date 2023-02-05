a7585130f687e537ed2126347947dabd9f2b3e95
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-02-05 06:51:52 -0400

    Visualización de todas las imágenes de los problemas

--- src/app/tests/problem/problem.component.ts
+++ src/app/tests/problem/problem.component.ts
@@ -3,17 +3,40 @@ import { Problem } from '../models/problem-model';
 
 @Component({
   selector: 'app-problem',
-  templateUrl: './problem.component.html'
+  templateUrl: './problem.component.html',
+  styles:[ `
+    .right-img {
+      max-width: 180px !important;
+      height: auto !important;
+    }
+    .option-img {
+      max-width: 100px !important;
+    }
+
+  `]
 })
 export class ProblemComponent implements OnInit {
 
   @Input() problem: Problem;
   public body_problem: string;
-
+  public right_img_url: string;
+  public decode_statement: string;
   constructor() { }
 
   ngOnInit(): void {
-    this.body_problem = `<b>${this.problem.num_s}.</b> ${this.problem.statement}`;
+    this.decode_statement = this.problem.statement;
+    let n=0;
+    for(let i=0; i<this.problem.statement.length; i++) {
+      if(this.problem.statement[i] == '{' && this.problem.statement[i+1] == '*' && (this.problem.statement[i+4] == '}' || this.problem.statement[i+5] == '}')) {
+        this.decode_statement = this.decode_statement.replace(`{*${n+1}*}`, `<img src="${this.problem.figures[n].url}">`)
+        n++;
+      }
+    }
+    this.body_problem = `<b>${this.problem.num_s}.</b> ${this.decode_statement}`;
+    for(let i=this.problem.figures.length-1; i>=0; i--) {
+      if(this.problem.figures[i].ubication === 'derecha')
+        this.right_img_url = this.problem.figures[i].url;
+    }
   }
 
   isLink(answer: string) {
