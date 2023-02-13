55c3b12ff8d5e830a6b10c491e75c6def728ea71
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-02-13 08:49:59 -0400

    Generación automática de usuarios con todos los campos

--- mySeedScript.js
+++ mySeedScript.js
@@ -25,23 +25,23 @@ async function seedDB() {
         // Make sure you run it against proper database and collection.
         collection.drop();
 
-        for (let i = 0; i < 1; i++) {
-            const registration_date = faker.date.between('2023-01-01T00:00:00.000Z', new Date());
+        for (let i = 0; i < 17561; i++) { //17561
+            const registration_date = faker.date.between('2023-02-01T00:00:00.000Z', new Date());
             const sex = faker.helpers.arrayElement(['F', 'M']);
-            var name, last_name, country, state, type, level, ci, type_institution;
+            var name, last_name, country, state, type, level, ci, type_institution, in_progress, t_remain_s;
             sex=='F'? name = faker.name.firstName('female') : name = faker.name.firstName('male');
             sex=='F'? last_name = faker.name.lastName('female') : last_name = faker.name.lastName('male');
             const username = name[0] + last_name;         
             const email = faker.internet.email(name, last_name, 'kanguromath.com');
             const date_birth = faker.date.birthdate({ min: 10, max: 25, mode: 'age' });
-            faker.helpers.maybe(() => country = 'venezuela', { probability: 0.8 })
-            faker.helpers.maybe(() => country = faker.helpers.arrayElement(["argentina", "bolivia", "brasil", "chile", "colombia", "costa-rica", "cuba", "ecuador", "espana", "el-salvador", "guatemala", "haiti", "honduras", "mexico", "nicaragua", "panama", "paraguay", "peru", "puerto-rico", "republica-dominicana", "uruguay"]), { probability: 0.2 })
+            faker.helpers.maybe(() => country = 'venezuela', { probability: 0.9 })
+            faker.helpers.maybe(() => country = faker.helpers.arrayElement(["argentina", "bolivia", "brasil", "chile", "colombia", "costa-rica", "cuba", "ecuador", "espana", "el-salvador", "guatemala", "haiti", "honduras", "mexico", "nicaragua", "panama", "paraguay", "peru", "puerto-rico", "republica-dominicana", "uruguay"]), { probability: 0.1 })
             if(country === 'venezuela') {
                 state = faker.helpers.arrayElement(["amazonas", "anzoategui", "apure", "aragua", "barinas", "bolivar", "carabobo", "cojedes", "delta-amacuro", "distrito-capital", "falcon", "guarico", "la-guaira", "lara", "merida", "miranda", "monagas", "nueva-esparta", "portuguesa", "sucre", "tachira", "trujillo", "yaracuy", "zulia"]);
             } else {
                 state = null;
             }
-            const streak_days = faker.datatype.number(100);
+            const streak_days = faker.datatype.number(10);
             if(date_birth.getFullYear() > 2000) {
                 type = 'estudiante';
             } else {
@@ -61,6 +61,20 @@ async function seedDB() {
                 type_institution = null;
             }
             const password = faker.internet.password(12, true);
+
+            const achievement = faker.helpers.arrayElement(["foto-perfil", "10-problemas", "1era-prueba", "1semana-racha", "cada-categoria", "1prueba-simulacion", "1mes-racha", "6meses-racha", "1ano-racha"]);
+            const percentage = faker.datatype.float({ min: 0, max: 1, precision: 0.01 }) 
+
+            const test = faker.helpers.arrayElement(["preliminar-2010-1ero-2do", "preliminar-2010-3ero", "preliminar-2013-1ero-2do", "preliminar-2013-1ero-2do", "preliminar-2011-1ero-2do"]);
+            const mode = faker.helpers.arrayElement(["practica", "simulacion"]);
+            if(mode == 'simulacion') {
+                in_progress = faker.datatype.boolean();
+                t_remain_s = faker.datatype.float({ min: 0, max: 6300 });
+            } else {
+                in_progress = false;
+                t_remain_s = 0;
+            }
+
             await collection.insertOne({
                 'registration_date': registration_date,
                 'name': name, 
@@ -76,7 +90,17 @@ async function seedDB() {
                 'level': level,
                 'ci': ci,
                 'type_institution': type_institution,
-                'password': password
+                'password': password,
+                'achieves': {
+                    'achievement': achievement,
+                    'percentage': percentage
+                },
+                'submit': {
+                    'test': test,
+                    'mode': mode,
+                    'in_progress': in_progress,
+                    't_remain_s': t_remain_s
+                }
             });
         }
         setTimeout(() => {client.close()}, 1500)
