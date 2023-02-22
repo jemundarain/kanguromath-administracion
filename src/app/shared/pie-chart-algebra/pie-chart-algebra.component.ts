77e6a6fda3d8600fbbb477947c9c034001ca5634
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-02-21 20:16:54 -0400

    Creación de pie chart de algebra

---
+++ src/app/shared/pie-chart-algebra/pie-chart-algebra.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartConfiguration, ChartItem } from 'chart.js/auto';
+import { GlobalConstants } from 'src/app/common/global-constants';
+import { Ranking } from 'src/app/pages/interfaces/ranking.interfaces';
+import { PagesService } from 'src/app/pages/services/pages.service';
+
+@Component({
+  selector: 'app-pie-chart-algebra',
+  templateUrl: './pie-chart-algebra.component.html'
+})
+export class PieChartAlgebraComponent implements OnInit {
+
+  constructor(private pagesService: PagesService) { }
+  
+  data: any;
+  distributionByPerformance: Ranking[];
+  
+  ngOnInit() {
+    this.pagesService.getAlgebraPerformanceDistribution('2023-01-01', '2023-02-20').subscribe(distributionByPerformance => {
+      this.distributionByPerformance = distributionByPerformance;
+      this.data = {
+        labels: GlobalConstants.getLabelsDistribution(this.distributionByPerformance),
+        datasets: [{
+          data: GlobalConstants.convertDistributionToArray(this.distributionByPerformance),
+          backgroundColor: ["#44896A", "#D93661"]
+        }]
+      };
+    })
+  }
+
+}
