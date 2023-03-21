04dada44b7f2a17421b85e50f3f6f6687f8ea6b7
José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2023-03-20 21:15:07 -0400

    Mejoras en el edit test

--- src/app/tests/edit-test/edit-test.component.ts
+++ src/app/tests/edit-test/edit-test.component.ts
@@ -7,12 +7,13 @@ import { Router } from '@angular/router';
 import { ActivatedRoute} from '@angular/router';
 import { switchMap } from 'rxjs';
 import { GlobalConstants } from 'src/app/common/global-constants';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MenuItem, MessageService } from 'primeng/api';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-edit-test',
   templateUrl: './edit-test.component.html',
-  providers: [MessageService]
+  providers: [ConfirmationService, MessageService]
 })
 export class EditTestComponent implements OnInit {
 
@@ -22,7 +23,7 @@ export class EditTestComponent implements OnInit {
 
   levels: RadioOption[];
   selectedLevelCode: string;
-
+  items: MenuItem[];
   test!: Test;
 
   @ViewChild('editTestForm') editTestForm!: NgForm;
@@ -30,22 +31,41 @@ export class EditTestComponent implements OnInit {
   constructor( private testService: TestService,
                private router: Router,
                private activatedRoute: ActivatedRoute,
+               private confirmationService: ConfirmationService,
+               private location: Location,
                private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.levels = GlobalConstants.LEVELS;
 
+    
     this.activatedRoute.params
-      .pipe(
-        switchMap( ({ id }) => this.testService.getTestById(id))
+    .pipe(
+      switchMap( ({ id }) => this.testService.getTestById(id))
       )
-      .subscribe( test => this.test = test );
+      .subscribe( test => {
+        this.test = test;
+        this.items = [
+          {label: 'Pruebas'},
+          {label: 'Editar Prueba'},
+          {label: `Preliminar ${this.test.edition} ${this.test.levels}`}
+        ];
+      });
   }
   
   updateTest() {
-    const test = new Test(this.test._id, this.test.test_id, this.editTestForm?.form.value.levels, this.editTestForm?.form.value.edition, this.test.problems);
-    this.testService.updateTest(test);
-    this.router.navigateByUrl('pruebas/lista');
-    this.messageService.add({severity:'success', summary: 'Exitoso', detail: 'Prueba editada'});
+    this.confirmationService.confirm({
+      header: "Confirmación",
+      message: '¿Está seguro que desea editar esta prueba?',
+      accept: () => {
+        console.log(this.editTestForm?.form.value.state);
+        const test = new Test(this.test._id, this.test.test_id, this.editTestForm?.form.value.levels, this.editTestForm?.form.value.edition, this.editTestForm?.form.value.state, this.test.problems);
+        this.testService.updateTest(test);
+        this.messageService.add({severity:'success', summary: 'Exitoso', detail: 'Prueba editada'});
+        setTimeout(() => {
+          this.location.back()
+        }, 1220);
+      }
+    });
   }
 }
